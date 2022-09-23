f1fd1145a107a97aa6d110a32b703909d111d99e
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-09-23 12:16:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function validation(){
   var text = document.getElementById('text');
 
   var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+if(email.match(pattern)) {
+   form.classList.add('valid');
+   form.classList.remove('invalid');
+   text.innerHTML = "Your email Address is vaild";
+   text.style.color = "#00ff00";
+} else{
+   form.classList.remove('valid');
+   form.classList.add('invalid');
+   text.innerHTML = "please enter valid email";
+   text.style.color = "#ff0000";
+} 
