efb8a4f8807db0d3b03764a83b87b5a9999cacaf
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-09-23 12:15:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ function validation(){
   var form = document.getElementById('form');
   var email = document.getElementById('email').value;
   var text = document.getElementById('text');
+
+  var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
