e374e81f637485bb697b45f33ab71c1376d08eef
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-09-23 12:17:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ if(email.match(pattern)) {
    text.innerHTML = "please enter valid email";
    text.style.color = "#ff0000";
 } 
+if(email == "") {
+   form.classList.remove('valid');
+   form.classList.remove('invalid');
+   text.innerHTML = "";
+   text.style.color = "#00ff00";
+}
+};
+  
